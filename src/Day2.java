f911f8c32e6beab0ce76615a169f6a382dde89d1
PLeh2023 <philipp.lehmann@nexus-ag.de>
2024-12-13 13:06:41 +0100

    miscellaneous

--- src/Day2.java
+++ src/Day2.java
@@ -1,5 +1,4 @@
 import Input.InputReader;
-
 import java.util.ArrayList;
 
 public class Day2 {
@@ -9,9 +8,7 @@ public class Day2 {
     }
     public static int run(int choice){
         InputReader inputReader = new InputReader();
-
         return countSafeReport(splitStringArrayIntoIntArray(inputReader.input("Inputs/Day2_Input.txt")),choice);
-
     }
     public static ArrayList<ArrayList<Integer>> splitStringArrayIntoIntArray(ArrayList<String> lists){
         ArrayList<ArrayList<Integer>> reports = new ArrayList<>(lists.size());
@@ -27,78 +24,50 @@ public class Day2 {
     }
     public static int countSafeReport(ArrayList<ArrayList<Integer>> reports, int choice){
         int safeReports = 0;
-        if (choice == 1) {
-            for (ArrayList<Integer> report : reports) {
-                if (report.get(0) < report.get(1)) {
-                    if (isSafeReport(report, false)) safeReports++;
-                } else if (report.get(0) > report.get(1)) {
-                    if (isSafeReport(report, true)) safeReports++;
-                }
-            }
-        }else{
-            for (ArrayList<Integer> report : reports) {
-                ArrayList<Integer> report2 = new ArrayList<>(report);
-                if (report.get(0) < report.get(1)) {
-                    if (isSafeReportWithFailure(report2, false)) safeReports++;
-                    else if (isSafeReportWithFailure2(report, false)) safeReports++;
-                } else if (report.get(0) > report.get(1)) {
-                    if (isSafeReportWithFailure(report2, true)) safeReports++;
-                    else if (isSafeReportWithFailure2(report, true)) safeReports++;
-                }
+        for (ArrayList<Integer> report : reports) {
+            if (choice == 1){
+                if (isSafeReport(report)) safeReports++;
+            }else{
+                if (isSafeReportWithFailure(report)) safeReports++;
             }
         }
         return safeReports;
     }
-    public static boolean isSafeReport(ArrayList<Integer> report, boolean desc){
-        for (int i = 0; i < report.size()-1; i++){
-            if (desc){
-                if (!(0 < report.get(i) - report.get(i + 1) && 4 > report.get(i) - report.get(i + 1))){
+    public static boolean isSafeReport(ArrayList<Integer> report){
+        if (isDescending(report.get(0),report.get(1))){
+            for (int i = 0; i < report.size()-1; i++) {
+                if (!(isSafeStepDescending(report.get(i), report.get(i + 1)))){
                     return false;
                 }
-            }else{
-                if (!(0 < report.get(i + 1) - report.get(i) && 4 > report.get(i + 1) - report.get(i))){
+            }
+        }else {
+            for (int i = 0; i < report.size()-1; i++) {
+                if (!(isSafeStepAscending(report.get(i), report.get(i + 1)))){
                     return false;
                 }
             }
         }
         return true;
     }
-    public static boolean isSafeReportWithFailure(ArrayList<Integer> report, boolean desc){
-        int counter = 0;
-        for (int i = 0; i < report.size()-1; i++){
-            if (desc){
-                if (!(0 < report.get(i) - report.get(i + 1) && 4 > report.get(i) - report.get(i + 1))){
-                    report.remove(i);
-                    counter++;
-                    i=-1;
-                }
-            }else{
-                if (!(0 < report.get(i + 1) - report.get(i) && 4 > report.get(i + 1) - report.get(i))){
-                    report.remove(i);
-                    counter++;
-                    i=-1;
-                }
+    public static boolean isSafeReportWithFailure(ArrayList<Integer> report){
+        if (isSafeReport(report)) return true;
+        for (int i = 0; i < report.size(); i++) {
+            ArrayList<Integer> report2 = new ArrayList<>(report);
+            report2.remove(i);
+            if (isSafeReport(report2)) {
+                return true;
             }
         }
-        return counter <= 1;
+
+        return false;
     }
-    public static boolean isSafeReportWithFailure2(ArrayList<Integer> report, boolean desc){
-        int counter = 0;
-        for (int i = 0; i < report.size()-1; i++){
-            if (desc){
-                if (!(0 < report.get(i) - report.get(i + 1) && 4 > report.get(i) - report.get(i + 1))){
-                    report.remove(i+1);
-                    counter++;
-                    i=-1;
-                }
-            }else{
-                if (!(0 < report.get(i + 1) - report.get(i) && 4 > report.get(i + 1) - report.get(i))){
-                    report.remove(i+1);
-                    counter++;
-                    i=-1;
-                }
-            }
-        }
-        return counter <= 1;
+    public static boolean isDescending(int a, int b){
+        return a > b;
+    }
+    public static boolean isSafeStepDescending(int a, int b){
+        return 0 < a-b && 4 > a-b;
+    }
+    public static boolean isSafeStepAscending(int a, int b){
+        return 0 < b-a && 4 > b-a;
     }
 }
