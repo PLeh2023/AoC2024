2f8906d40804e969d77ce7bb219ac714d44da445
user name <user>
2025-02-28 09:43:11 +0100

    miscellaneous

--- src/Day4.java
+++ src/Day4.java
@@ -1,6 +1,4 @@
 import Input.InputReader;
-
-import java.security.spec.ECField;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -16,48 +14,66 @@ public class Day4 {
         }
         return findX_Mas(inputReader.inputToIID("Inputs/Day4_Input.txt"));
     }
-    public static int findXMAS(ArrayList<ArrayList<String>> map){
-       int sum = 0;
-        for (int i = 0; i < map.size(); i++){
-           for (int j = 0; j < map.getFirst().size(); j++){
-               if (Objects.equals(map.get(i).get(j), "X")){
-                   //right
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i).get(j+1) + map.get(i).get(j+2) + map.get(i).get(j+3), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //right down
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i+1).get(j+1) + map.get(i+2).get(j+2) + map.get(i+3).get(j+3), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //down
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i+1).get(j) + map.get(i+2).get(j) + map.get(i+3).get(j), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //left down
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i+1).get(j-1) + map.get(i+2).get(j-2) + map.get(i+3).get(j-3), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //left
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i).get(j-1) + map.get(i).get(j-2) + map.get(i).get(j-3), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //left up
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i-1).get(j-1) + map.get(i-2).get(j-2) + map.get(i-3).get(j-3), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //up
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i-1).get(j) + map.get(i-2).get(j) + map.get(i-3).get(j), "XMAS")) {sum += 1;}}catch (Exception _){}
-                   //right up
-                   try {if (Objects.equals(map.get(i).get(j) + map.get(i-1).get(j+1) + map.get(i-2).get(j+2) + map.get(i-3).get(j+3), "XMAS")) {sum += 1;}}catch (Exception _){}
-               }
-           }
-       }
-        return sum;
-    }
-    public static int findX_Mas(ArrayList<ArrayList<String>> map){
+    public static int findXMAS(ArrayList<ArrayList<String>> map) {
         int sum = 0;
-        for (int i = 0; i < map.size(); i++){
-            for (int j = 0; j < map.getFirst().size(); j++){
-                if (Objects.equals(map.get(i).get(j), "A")){
-                    //right MMs
-                    try{if (Objects.equals(map.get(i-1).get(j+1) + map.get(i).get(j) + map.get(i+1).get(j-1), "MAS") && Objects.equals(map.get(i+1).get(j+1) + map.get(i).get(j) + map.get(i-1).get(j-1), "MAS")){sum += 1;}}catch (Exception _){}
-                    //down MMs
-                    try{if (Objects.equals(map.get(i+1).get(j+1) + map.get(i).get(j) + map.get(i-1).get(j-1), "MAS") && Objects.equals(map.get(i+1).get(j-1) + map.get(i).get(j) + map.get(i-1).get(j+1), "MAS")){sum += 1;}}catch (Exception _){}
-                    //left MMs
-                    try{if (Objects.equals(map.get(i+1).get(j-1) + map.get(i).get(j) + map.get(i-1).get(j+1), "MAS") && Objects.equals(map.get(i-1).get(j-1) + map.get(i).get(j) + map.get(i+1).get(j+1), "MAS")){sum += 1;}}catch (Exception _){}
-                    //up MMs
-                    try{if (Objects.equals(map.get(i-1).get(j-1) + map.get(i).get(j) + map.get(i+1).get(j+1), "MAS") && Objects.equals(map.get(i-1).get(j+1) + map.get(i).get(j) + map.get(i+1).get(j-1), "MAS")){sum += 1;}}catch (Exception _){}
+        int[][][] directions = {
+                {{0, 0}, {0, 1}, {0, 2}, {0, 3}},   // Rechts
+                {{0, 0}, {1, 1}, {2, 2}, {3, 3}},   // Diagonal rechts unten
+                {{0, 0}, {1, 0}, {2, 0}, {3, 0}},   // Unten
+                {{0, 0}, {1, -1}, {2, -2}, {3, -3}}, // Diagonal links unten
+                {{0, 0}, {0, -1}, {0, -2}, {0, -3}}, // Links
+                {{0, 0}, {-1, -1}, {-2, -2}, {-3, -3}}, // Diagonal links oben
+                {{0, 0}, {-1, 0}, {-2, 0}, {-3, 0}}, // Oben
+                {{0, 0}, {-1, 1}, {-2, 2}, {-3, 3}}  // Diagonal rechts oben
+        };
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.getFirst().size(); j++) {
+                if (Objects.equals(map.get(i).get(j), "X")) {
+                    for (int[][] dir : directions) {
+                        if (checkPattern(map, i, j, dir, "XMAS")) {
+                            sum++;
+                        }
+                    }
                 }
             }
         }
         return sum;
+    }
+    public static int findX_Mas(ArrayList<ArrayList<String>> map) {
+        int sum = 0;
+        int[][][] directions = {
+                {{-1, 1}, {0, 0}, {1, -1}}, // Diagonal rechts unten
+                {{1, 1}, {0, 0}, {-1, -1}}, // Diagonal links oben
+                {{1, -1}, {0, 0}, {-1, 1}}, // Diagonal links unten
+                {{-1, -1}, {0, 0}, {1, 1}}  // Diagonal rechts oben
+        };
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.getFirst().size(); j++) {
+                if (Objects.equals(map.get(i).get(j), "A")) {
+                    int count = 0;
+                    for (int[][] dir : directions) {
+                        if (checkPattern(map, i, j, dir, "MAS")) {
+                            count++;
+                        }
+                    }
+                    if (count >= 2) {
+                        sum++;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+    public static boolean checkPattern(ArrayList<ArrayList<String>> map, int i, int j, int[][] directions, String pattern) {
+        StringBuilder foundPattern = new StringBuilder();
+        for (int[] dir : directions) {
+            int newRow = i + dir[0];
+            int newCol = j + dir[1];
+            if (newRow < 0 || newRow >= map.size() || newCol < 0 || newCol >= map.getFirst().size()) {
+                return false;
+            }
+            foundPattern.append(map.get(newRow).get(newCol));
+        }
+        return foundPattern.toString().equals(pattern);
     }
 }
